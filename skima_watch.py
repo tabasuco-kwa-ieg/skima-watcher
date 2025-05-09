2ce5f7c15950815d3b8844f97e5144ae428d8e11
ubume_tamago <58930677+tabasuco-kwa-ieg@users.noreply.github.com>
2025-05-10 08:30:21 +0900

    デバック用追記

--- skima_watch.py
+++ skima_watch.py
@@ -3,7 +3,7 @@ import os, requests, bs4, shelve, pathlib
 SELLER_ID = "151777"                      # ←監視したい出品者のID
 BASE_URL  = f"https://skima.jp/profile/dl_products?id={SELLER_ID}"
 IFTTT_KEY = os.getenv("IFTTT_KEY")        # GitHub Secrets から取得
-IFTTT_URL = f"{IFTTT_KEY}"
+IFTTT_URL = f"https://maker.ifttt.com/trigger/skima_opt_new/with/key/{IFTTT_KEY}"
 
 # .github/actions/cache に保存すると GitHub Actions 側で永続化しやすい
 CACHE     = pathlib.Path(".cache")
@@ -28,3 +28,8 @@ with shelve.open(DB_PATH) as db:
 
     if items != prev:
         db["items"] = list(items)
+
+    if not items - prev:
+        print("（テスト送信）差分が無いのでダミーを送ります")
+        requests.post(IFTTT_URL, json={"content": "Ping from CI"})
+
